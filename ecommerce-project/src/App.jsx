54878d5db1740b25e8a19dc9b07edc1311491c2b
Justin Hawkins <jscott3198@gmail.com>
2025-11-20 11:38:18 -0600

    created checkout page, and added a route component with the element as the checkckoutPage component in the Routes componoent, and deleted some of the checkoutPage.html from the starting-code folder, bc it would transitioned to react in the CheckoutPage componenet page, as well as its respective css

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { HomePage } from "./Pages/HomePage";
 import { Routes, Route } from "react-router";
+import { CheckoutPage } from "./Pages/CheckoutPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="checkout" element={<div>test checkout page </div>}></Route>
+      <Route index element={<HomePage />}></Route>
+      <Route path="checkout" element={<CheckoutPage />}></Route>
     </Routes>
   );
 }
