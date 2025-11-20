b5653134be1328141cfdc1ac0ce9c67d8c604aa3
Justin Hawkins <jscott3198@gmail.com>
2025-11-20 11:57:13 -0600

    moved over and updated the orders pages & css

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,15 @@
 import { HomePage } from "./Pages/HomePage";
 import { Routes, Route } from "react-router";
 import { CheckoutPage } from "./Pages/CheckoutPage";
+import { OrdersPage } from "./Pages/OrdersPage";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />}></Route>
-      <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }
