6ef54b2cc53c9440161d40b2c47d6b1ba0c24323
Justin Hawkins <jscott3198@gmail.com>
2025-12-05 08:21:27 -0600

    fixed a bug in the payment summary, and Delivery options  components

--- ecommerce-project/src/Pages/checkout/PaymentSummary.jsx
+++ ecommerce-project/src/Pages/checkout/PaymentSummary.jsx
@@ -30,7 +30,7 @@ export function PaymentSummary({ paymentSummary }) {
           <div className="payment-summary-row">
             <div>Estimated tax (10%):</div>
             <div className="payment-summary-money">
-              formatMoney(paymentSummary.taxCents)
+              {formatMoney(paymentSummary.taxCents)}
             </div>
           </div>
 
